2237e27acee82098b6981db2f52b33c2a4748d7e
Arbousier1 <elderli@foxmail.com>
2026-02-17 14:20:07 +0800

    额

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:${rootProject.property("junitVersion")}")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
